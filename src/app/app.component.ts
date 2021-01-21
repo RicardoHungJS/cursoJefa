bd1e1239980504ac6e1ed14b689550a1a2d733e5
Ricardo Hung <70046988+RicardoHungJS@users.noreply.github.com>
2021-01-21 13:45:18 -0500

    clean app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,29 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  valor1: number;
-  valor2: number;
-  valor3: number;
-  resultado: string;
-
-  constructor(){
-    this.valor1 = this.retornarAleatorio();
-    this.valor2 = this.retornarAleatorio();
-    this.valor3 = this.retornarAleatorio();
-  }
-
-  retornarAleatorio() {
-    return Math.trunc(Math.random() * 6) + 1;
-  }
-
-  tirar() {
-    this.valor1 = this.retornarAleatorio();
-    this.valor2 = this.retornarAleatorio();
-    this.valor3 = this.retornarAleatorio();
-    if (this.valor1==this.valor2 && this.valor1==this.valor3)    
-      this.resultado='Ganó';
-    else
-      this.resultado='Perdió';
-  }
-}
+export class AppComponent {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DadoComponent } from './components/dado/dado.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DadoComponent
   ],
   imports: [
     BrowserModule,
