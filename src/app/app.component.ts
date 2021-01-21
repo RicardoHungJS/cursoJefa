cf08c036f26d6269573d8cee23447da587b91b70
Ricardo Hung <70046988+RicardoHungJS@users.noreply.github.com>
2021-01-21 12:34:14 -0500

    modulo terminado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'cursoJefa';
+  mensaje = '';
+
+  actualizar(t: any) {
+    this.mensaje = t + '(se actualiza cada 10 segundos)';
+  }
 }
