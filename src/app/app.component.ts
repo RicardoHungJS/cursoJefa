bcc6ac3d18785f0e8248b7ded4a4f4922f9b5246
Ricardo Hung <70046988+RicardoHungJS@users.noreply.github.com>
2021-01-22 10:04:03 -0500

    modulo terminado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,4 +5,18 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {}
+export class AppComponent {
+  nombre = 'Ricardo Hung';
+  saldo = 1000.05;
+  dias = ['domingo', 'lunes', 'martes', 'miércoles', 'jueves', 'viernes', 'sábado'];
+  articulos = [{
+    codigo: 1,
+    descripcion: "papas",
+    precio: 12.33
+  }, {
+    codigo: 2,
+    descripcion: "manzanas",
+    precio: 54
+  }];
+  fechaActual = new Date();
+}
