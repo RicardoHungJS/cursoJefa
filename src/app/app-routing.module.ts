bc174a7c21c7f6be6d8c78ebd961c2ceadb04b17
Ricardo Hung <70046988+RicardoHungJS@users.noreply.github.com>
2021-01-25 09:06:54 -0500

    pequeños cambios

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,12 +7,12 @@ const routes: Routes = [
   {
     path: 'america',
     component: AmericaComponent,
-    loadChildren: () => import('./modules/america/america.module').then(m => m.AmericaModule)
+    loadChildren: () => import('./modules/america/america.module').then(mod => mod.AmericaModule)
   },
   {
     path: 'europa',
     component: EuropaComponent,
-    loadChildren: () => import('./modules/europa/europa.module').then(m => m.EuropaModule)
+    loadChildren: () => import('./modules/europa/europa.module').then(mod => mod.EuropaModule)
   }
 ];
 
