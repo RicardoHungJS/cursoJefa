4941881de29bd1f048907cdbc55efc8fcbca00a9
Ricardo Hung <70046988+RicardoHungJS@users.noreply.github.com>
2021-01-25 11:01:24 -0500

    Modulo terminado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,25 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {}
+export class AppComponent {
+  datos: string;
+
+  formularioContacto = new FormGroup({
+    nombre: new FormControl(''),
+    mail: new FormControl(''),
+    mensaje: new FormControl('')
+  });
+
+  submit(){
+    this.datos=`
+    Nombre= ${this.formularioContacto.value.nombre}
+    Mail= ${this.formularioContacto.value.mail}
+    mensaje= ${this.formularioContacto.value.mensaje}
+    `
+  }
+}
